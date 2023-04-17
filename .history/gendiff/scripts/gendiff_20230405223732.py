5e17b9a79d8eca698e5152bf0752708907e1290c
Bkorob <bakatest311@yandex.ru>
2023-04-18 00:43:58 +0300

    дополнение .gitignode

--- .history/gendiff/scripts/gendiff_20230405223732.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-from cli import parser
-from gendiff.modules.gendiff import generate_result
-
-
-def main():
-    generate_result(parser.firs_file, parser.second_file)
--- .history/gendiff/scripts/gendiff_20230405223926.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-from cli import parser
-from gendiff.modules.gendiff import generate_result
-
-
-def main():
-    generate_result(parser.add_argument('first_file'), 
-                    parser.add_argument('second_file')
-                    )
