59a340377127e10e21711aa69be152fdebd7e562
Bkorob <bakatest311@yandex.ru>
2023-04-22 01:48:50 +0300

    3шаг

--- gendiff/modules/gendiff.py
+++ gendiff/modules/gendiff.py
@@ -1,28 +1,28 @@
 import json
 
+first_file = json.load(open('gendiff/files/json/file1.json'))
+second_file = json.load(open('gendiff/files/json/file2.json'))
 
-def generate_result():
-    first_file = json.load(open('gendiff/files/json/file1.json'))
-    second_file = json.load(open('gendiff/files/json/file2.json'))
+def generate_result(first_file, second_file): 
     result = {}
-    for first_key in first_file:
-        if first_key in second_file and first_file[first_key] == second_file[first_key]:
-            result["  " + first_key] = first_file[first_key]
-        elif first_key in second_file and first_file[first_key] != second_file[first_key]:
-            result["- " + first_key] = first_file[first_key]
-            result["+ " + first_key] = second_file[first_key]
+    for key1 in first_file:
+        if key1 in second_file and first_file[key1] == second_file[key1]:
+            result["  " + key1] = first_file[key1]
+        elif key1 in second_file and first_file[key1] != second_file[key1]:
+            result["- " + key1] = first_file[key1]
+            result["+ " + key1] = second_file[key1]
         else:
-            result["- " + first_key] = first_file[first_key]
-    for second_key in second_file:
-        if second_key not in first_file:
-            result["+ " + second_key] = second_file[second_key]
+            result["- " + key1] = first_file[key1]
+    for key2 in second_file:
+        if key2 not in first_file:
+            result["+ " + key2] = second_file[key2]
     result = dict(sorted(result.items(), key=lambda x: x[0][2]))
     return result
 
 
 def get_print(generate_result):
         print("{")
-        for key, val in generate_result().items():
+        for key, val in generate_result(first_file, second_file).items():
             if isinstance(val, bool):
                 print(key, ':', str(val).lower())
             else:
@@ -30,6 +30,11 @@ def get_print(generate_result):
         print("}")
 
 
-if __name__ == '__main__':
-    generate_result()
+def main():
+    
+    generate_result(first_file, second_file)
     get_print(generate_result)
+    
+
+if __name__ == '__main__':
+    main()
---
+++ gendiff/scripts/generate.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from modules.gendiff import get_print, generate_result
+from modules.cli import parse_arguments
+
+
+def main():
+    
+    get_print(generate_result)
