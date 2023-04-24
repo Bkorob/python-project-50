c03d6f48d9923613af9514d8012d8550cf66b8bb
Bkorob <bakatest311@yandex.ru>
2023-04-24 15:00:36 +0300

    упрощение файловой структуры

--- gendiff/scripts/generate.py
+++ gendiff/scripts/generate.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-from modules.gendiff import get_print, generate_result
+from gendiff.gendiff import get_print, generate_result
 from modules.cli import parse_arguments
 
 
-def main():
+
     
-    get_print(generate_result)
+get_print(generate_result(parse_arguments))
