394d9412f7e87bfb8f1279b11f2890389554f574
Bkorob <bakatest311@yandex.ru>
2023-04-26 02:21:33 +0300

    Шаг3 фикс ошибок flake

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -2,11 +2,8 @@ import json
 
 
 def generate_result(file1, file2):
-    # file1 = json.load(open(first_file))
-    # file2 = json.load(open(second_file))
-       
     result = {}
-    
+
     for key1 in file1:
         if key1 in file2 and file1[key1] == file2[key1]:
             result[f"  {key1}"] = file1[key1]
@@ -15,20 +12,22 @@ def generate_result(file1, file2):
             result[f"+ {key1}"] = file2[key1]
         else:
             result[f"- {key1}"] = file1[key1]
+
     for key2 in file2:
         if key2 not in file1:
             result[f"+ {key2}"] = file2[key2]
+
     result = dict(sorted(result.items(), key=lambda x: x[0][2]))
     return result
 
 
 def get_dict_print(first_file, second_file):
-        file1 = json.load(open(first_file))
-        file2 = json.load(open(second_file))
-        print("{")
-        for key, val in generate_result(file1, file2).items():
-            if isinstance(val, bool):
-                print(key, ':', str(val).lower())
-            else:
-                print(key, ':', val)
-        print("}")
+    file1 = json.load(open(first_file))
+    file2 = json.load(open(second_file))
+    print("{")
+    for key, val in generate_result(file1, file2).items():
+        if isinstance(val, bool):
+            print(key, ':', str(val).lower())
+        else:
+            print(key, ':', val)
+    print("}")
--- gendiff/scripts/generate.py
+++ gendiff/scripts/generate.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-from gendiff.generate_diff import get_dict_print, generate_result
+from gendiff.generate_diff import get_dict_print
 from gendiff.cli import parse_arguments
-
 
 
 def main():
@@ -9,7 +8,7 @@ def main():
     file_path1 = pathes.first_file
     file_path2 = pathes.second_file
     get_dict_print(file_path1, file_path2)
-    
+
 
 if __name__ == '__main__':
     main()
