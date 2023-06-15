77bc38080eb81b16122fc28d7b3ed4df4cebba38
Bkorob <bakatest311@yandex.ru>
2023-06-15 15:13:07 +0300

    ШАГ4: Натписание тестов

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,17 +1,28 @@
 from gendiff import generate_diff
-# import os
 
-# with open(os.path.dirname(os.path.abspath(__file__))
-#           + "/tests/fixtures/correct_result.txt", "r", encoding='utf-8') as f:
-file1 = './tests/fixtures/file1.json'
-file2 = './tests/fixtures/file1.json'
-   
-with open('./tests/fixtures/correct_result.txt') as f:
-        result = f.read()
-print(result)
-a = generate_diff(file1, file2)
-print(a)
-def test_generate():
+
+def test_generate_some():
+    file1 = './tests/fixtures/file1.json'
+    file2 = './tests/fixtures/file2.json'
     with open('./tests/fixtures/correct_result.txt') as f:
         result = f.read()
-        assert result == generate_diff(file1, file2)
+        diff = generate_diff(file1, file2)
+        assert result == diff
+
+
+def test_generate_different():
+    file1 = './tests/fixtures/file1.json'
+    file2 = './tests/fixtures/file3.json'
+    with open('./tests/fixtures/correct_result2.txt') as f:
+        result = f.read()
+        diff = generate_diff(file1, file2)
+        assert result == diff
+
+
+def test_generate_same():
+    file1 = './tests/fixtures/file1.json'
+    file2 = './tests/fixtures/file1.json'
+    with open('./tests/fixtures/correct_result3.txt') as f:
+        result = f.read()
+        diff = generate_diff(file1, file2)
+        assert result == diff
