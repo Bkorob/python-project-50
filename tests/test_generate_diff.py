830b85660836ef93d6a3ffc1cb3cd2f21e2a54c5
Bkorob <bakatest311@yandex.ru>
2023-07-29 17:14:43 +0300

    fixed bugs

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -35,3 +35,11 @@ def test_yml_generate_diff():
         result = cr.read()
         diff = generate_diff(file1, file2)
         assert result == diff
+        
+def test_yaml_different_format()
+    file1 = "../tests/fixtures/file1.yml"
+    file2 = "../tests/fixtures/file2.yaml"
+    with open("../tests/fixtures/correct_result.txt") as cr:
+        result = cr.read()
+        diff = generate_diff(file1, file2)
+        assert result == diff
