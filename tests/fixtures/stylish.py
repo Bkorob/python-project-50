67ee91f753bb0a176e551bfc051948ae2b7d24f9
Bkorob <bakatest311@yandex.ru>
2023-08-29 15:08:17 +0300

    Шаг6 фикс багов  в тестах

--- tests/fixtures/stylish.py
+++ tests/fixtures/stylish.py
@@ -1,8 +1,8 @@
-def stylish(data, tabulation='', depth=1):
-    result = []
-    if depth == 1:
-        indent = tabulation * 4 
-    result.append('{\n')
-    for key in data:
-        result.append(data[key]: )
+# def stylish(data, tabulation='', depth=1):
+#     result = []
+#     if depth == 1:
+#         indent = tabulation * 4 
+#     result.append('{\n')
+#     for key in data:
+#         result.append(data[key]: )
         
