66bc2a844b95291a1d57f2113bb63bd090b02e03
Bkorob <bakatest311@yandex.ru>
2023-08-29 14:53:20 +0300

    Шаг6: фикс generate для flat, доописаны тесты

--- tests/fixtures/stylish.py
+++ tests/fixtures/stylish.py
@@ -1,14 +1,8 @@
-def stringify(value, replacer=' ', spaces_count=1, _level=1):
-    if not isinstance(value, dict):
-        return str(value)
-    else:
-        indent = replacer * spaces_count * _level
-        result = '{\n'
-        for k, v in value.items():
-            result += f'{indent}{k}: '
-            result += f'{stringify(v, replacer, spaces_count, _level+1)}\n'
-        result += f'{replacer * spaces_count * (_level - 1)}}}'
-        return result
-    
-data = {"hello": "world", "is": True, "nested": {"count": 5}}    
-print(stringify(data, '|-', 2))
+def stylish(data, tabulation='', depth=1):
+    result = []
+    if depth == 1:
+        indent = tabulation * 4 
+    result.append('{\n')
+    for key in data:
+        result.append(data[key]: )
+        
