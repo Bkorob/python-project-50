1952d13ec22509b22a9383672273caa9fb944b19
Bkorob <bakatest311@yandex.ru>
2023-09-06 17:58:35 +0300

    Шаг6: внесены изменения в ФС

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,4 +1,4 @@
 from .generate import generate_diff
 from .cli import parse_arguments
-from .formatters import stylish
+from .formatters.stylish import stylish
 __all__ = ('generate_diff', 'parse_arguments', 'stylish')
