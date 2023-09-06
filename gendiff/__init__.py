ef14b9769af0abdf0fb9b3099f29cd71317b6c11
Bkorob <bakatest311@yandex.ru>
2023-09-06 13:54:20 +0300

    Шаг6: приведение ФС в порядок

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,4 +1,4 @@
 from .generate import generate_diff
 from .cli import parse_arguments
-
-__all__ = ('generate_diff', 'parse_arguments')
+from .formatters import stylish
+__all__ = ('generate_diff', 'parse_arguments', 'stylish')
