660dee50a596e8b0d8108e1b97b7c384c0e01fc5
Bkorob <bakatest311@yandex.ru>
2023-09-09 15:07:35 +0300

    Шаг6: переделка внутренней логики

--- gendiff/generate.py
+++ gendiff/generate.py
@@ -3,56 +3,83 @@ import yaml
 import re
 from .formatters.stylish import stylish
 
-def make_performance(file1, file2):
-    tree = []
+
+def make_inner_view(file1, file2):
+    tree = {}
     keys = file1.keys() | file2.keys()
     for key in sorted(keys):
         if key not in file2:
-            tree.append({
-                'key': key,
-                'value': file1[key],
-                'meta': 'deleted'
-            })
+            tree[key] = {}
+            tree[key]['value'] = file1[key]
+            tree[key]['meta'] = 'deleted'
         elif key in file1 and key in file2:
-            if all(map(lambda x: isinstance(x, dict), (file1[key], file2[key]))):
-                tree.append({
-                    'key': key,
-                    'value': make_performance(file1[key], file2[key]),
-                    'meta': 'children'
-                })
+            if isinstance(file1[key], dict) and isinstance(file2[key], dict):
+                tree[key] = {}
+                tree[key]['value'] = make_inner_view(file1[key], file2[key])
+                tree[key]['meta'] = 'children'
             elif file1[key] == file2[key]:
-                tree.append({
-                    'key': key,
-                    'value': file1[key],
-                    'meta': 'unchanged'
-                })
+                tree[key] = {}
+                tree[key]['value'] = file1[key],
+                tree[key]['meta'] = 'unchanged',
             elif file1[key] != file2[key]:
-                tree.append({
-                    'key': key,
-                    'value': file2[key],
-                    'meta': 'changed-'
-                })
-                tree.append({
-                    'key': key,
-                    'value': file1[key],
-                    'meta': 'changed+'
-                })                
+                tree[key] = {}
+                tree[key]['value'] = (file2[key], file1[key])
+                tree[key]['meta'] = 'changed'              
         else:
-            tree.append({
-                'key': key,
-                'value': file2[key],
-                'meta': 'added'
-            })
+            tree[key] = {}
+            tree[key]['value'] =  file2[key]
+            tree[key]['meta'] = 'added'
     return tree
+# def make_inner_view(file1, file2):
+#     tree = []
+#     keys = file1.keys() | file2.keys()
+#     for key in sorted(keys):
+#         if key not in file2:
+#             tree.append({
+#                 'key': key,
+#                 'value': file1[key],
+#                 'meta': 'deleted'
+#             })
+#         elif key in file1 and key in file2:
+#             if isinstance(file1[key], dict) and isinstance(file2[key], dict):
+#                 tree.append({
+#                     'key': key,
+#                     'value': make_inner_view(file1[key], file2[key]),
+#                     'meta': 'children'
+#                 })
+#             elif file1[key] == file2[key]:
+#                 tree.append({
+#                     'key': key,
+#                     'value': file1[key],
+#                     'meta': 'unchanged'
+#                 })
+#             elif file1[key] != file2[key]:
+#                 tree.append({
+#                     'key': key,
+#                     'value': file2[key],
+#                     'meta': 'changed-'
+#                 })
+#                 tree.append({
+#                     'key': key,
+#                     'value': file1[key],
+#                     'meta': 'changed+'
+#                 })                
+#         else:
+#             tree.append({
+#                 'key': key,
+#                 'value': file2[key],
+#                 'meta': 'added'
+#             })
+#     return tree
 
 
 def parse_file(path):
-    filepath = path
-    if filepath.endswith('yaml') or filepath.endswith('yml'):
+    filepath = path.split('.')
+    if filepath[-1] in ['yaml', 'yml']:
         with open(path) as p_y:
             source = yaml.safe_load(p_y)
             return source
-    elif filepath.endswith('json'):
+    elif filepath[-1] == 'json':
         with open(path) as p_j:
             source = json.load(p_j)
             return source
@@ -62,7 +89,7 @@ def parse_file(path):
 def generate_diff(first_file, second_file, formatter=stylish):
     file1 = parse_file(first_file)
     file2 = parse_file(second_file)
-    intermediate_result = make_performance(file1, file2)
+    intermediate_result = make_inner_view(file1, file2)
     result = formatter(intermediate_result)
     diff = re.sub(r'[",]', '', result)
     return diff
