714e337ea7aa8ea2f0a646eaa76db8f70c5616cf
Bkorob <bakatest311@yandex.ru>
2023-09-10 17:30:23 +0300

    new stylish

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -9,33 +9,57 @@ def stylish(data, indent_size=4, depth=0):
     result = []
     sign = ''
     indent = ''
-    result.append('{\n')
+    result.append('{')
     for elem in data:
-        if isinstance(elem, dict):
-            if elem['meta'] not in data:
-                sign = '  '
-            match elem['meta']:
-                case 'added':
-                    sign = '+ '
-                case 'deleted':
-                    sign = '- '
-                case 'unchanged':
-                    sign = '  '
-                case 'changed-':
-                    sign = '- '
-                case 'changed+':
-                    sign = '+ '    
+        if isinstance(data[elem], dict):   
             indent =  ' ' * (depth * indent_size-2) if depth > 0 else ' ' * (indent_size-2)
-            if isinstance(elem['value'], dict):
-                result.append(f'{indent}  {sign}{elem["key"]}: {stylish(elem["value"], indent_size, depth+1)}')
-            elif elem['meta'] == 'children':
-                result.append(f'{indent}  {sign}{elem["key"]}: {stylish(elem["value"], indent_size, depth +1)}')
+            if isinstance(data[elem]['value'], dict):
+                result.append(f'{indent}  {sign}{data[elem]["key"]}: {stylish(data[elem]["value"], indent_size, depth+1)}\n')
+            elif data[elem]['meta'] == 'children':
+                result.append(f'{indent}  {sign}{data[elem]["key"]}: {stylish(data[elem]["value"], indent_size, depth +1)}\n')
             else:
-                result.append(f'{indent}  {sign}{elem["key"]}: {convert_value(elem["value"])}\n')
+                result.append(f'{indent}  {sign}{data[elem]["key"]}: {convert_value(data[elem]["value"])}\n')
         else:
-            indent =  ' ' * (depth * indent_size-2) if depth > 0 else ' ' * (indent_size-2)
-            result.append(f'{indent}    {sign}{elem}: {convert_value(data[elem])}\n')
+            indent =  ' ' * (depth * indent_size) if depth > 0 else ' ' * (indent_size)
+            result.append(f'{indent}  {sign}{data[elem]} {data[elem]["value"]}\n')
     indent =  ' ' * (depth * indent_size-2)
-    result.append(indent + '}\n')           
+    result.append(indent + '}')           
     return ''.join(result)
+
+
+
+
+# def stylish(data, indent_size=4, depth=0):
+#     result = []
+#     sign = ''
+#     indent = ''
+#     result.append('{\n')
+#     for elem in data:
+#         if isinstance(elem, dict):
+#             if elem['meta'] not in data:
+#                 sign = '  '
+#             match elem['meta']:
+#                 case 'added':
+#                     sign = '+ '
+#                 case 'deleted':
+#                     sign = '- '
+#                 case 'unchanged':
+#                     sign = '  '
+#                 case 'changed-':
+#                     sign = '- '
+#                 case 'changed+':
+#                     sign = '+ '    
+#             indent =  ' ' * (depth * indent_size-2) if depth > 0 else ' ' * (indent_size-2)
+#             if isinstance(elem['value'], dict):
+#                 result.append(f'{indent}  {sign}{elem["key"]}: {stylish(elem["value"], indent_size, depth+1)}')
+#             elif elem['meta'] == 'children':
+#                 result.append(f'{indent}  {sign}{elem["key"]}: {stylish(elem["value"], indent_size, depth +1)}')
+#             else:
+#                 result.append(f'{indent}  {sign}{elem["key"]}: {convert_value(elem["value"])}\n')
+#         else:
+#             indent =  ' ' * (depth * indent_size-2) if depth > 0 else ' ' * (indent_size-2)
+#             result.append(f'{indent}    {sign}{elem}: {convert_value(data[elem])}\n')
+#     indent =  ' ' * (depth * indent_size-2)
+#     result.append(indent + '}\n')           
+#     return ''.join(result)
 
