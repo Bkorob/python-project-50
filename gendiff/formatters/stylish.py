391f5dd65e29e85842e3aac35f6ce534679ad08a
Bkorob <bakatest311@yandex.ru>
2023-09-10 18:06:18 +0300

    new stylish

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -5,26 +5,14 @@ def convert_value(value):
     return CONVERTING_VALUE.get(value, value)
 
 
-def stylish(data, indent_size=4, depth=0):
-    result = []
-    sign = ''
-    indent = ''
-    result.append('{')
+def stylish(data, depth=1): #пока идея такая 
+    result = ''
     for elem in data:
-        if isinstance(data[elem], dict):   
-            indent =  ' ' * (depth * indent_size-2) if depth > 0 else ' ' * (indent_size-2)
-            if isinstance(data[elem]['value'], dict):
-                result.append(f'{indent}  {sign}{data[elem]["key"]}: {stylish(data[elem]["value"], indent_size, depth+1)}\n')
-            elif data[elem]['meta'] == 'children':
-                result.append(f'{indent}  {sign}{data[elem]["key"]}: {stylish(data[elem]["value"], indent_size, depth +1)}\n')
-            else:
-                result.append(f'{indent}  {sign}{data[elem]["key"]}: {convert_value(data[elem]["value"])}\n')
-        else:
-            indent =  ' ' * (depth * indent_size) if depth > 0 else ' ' * (indent_size)
-            result.append(f'{indent}  {sign}{data[elem]} {data[elem]["value"]}\n')
-    indent =  ' ' * (depth * indent_size-2)
-    result.append(indent + '}')           
-    return ''.join(result)
+        if isinstance(data[elem], dict):
+                indent = '    ' * depth
+                result += f'{indent}{get_sign(data[elem])}{data[elem]["key"]}:{convert_value(data[elem]["value"])}\n'
+    result = '{\n' + result + f'{depth * "    "}' + '}'
+    return result
 
 
 
