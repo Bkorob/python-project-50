913544fdabd6e4602342ff77ba5ffa47fa091a36
Bkorob <bakatest311@yandex.ru>
2023-09-26 19:12:22 +0300

    Шаг7: plain

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -1,23 +1,33 @@
 from gendiff.formatters.stylish import convert_value
 
-
 def get_value(value):
     if isinstance(value, dict):
         return '[complex value]'
     return convert_value(value)
 
+
+def get_path(previous_path, new_part):
+    new_path = f'{previous_path}.{new_part}'
+    return new_path
+
+
 def plain(data, key_name=''):
     result = ''
     for elem in data:
-        key_name += f'.{elem["key"]}'
+        key = elem['key']
         value = elem['value']
         meta = elem['meta']
-        # if isinstance(value, dict):
-        #     result +=  
         if meta == 'children':
-            result += plain(value, key_name=elem['key']) 
+            result += plain(value, key_name=elem['key'])
+        elif meta == 'changed+' or meta == 'changed-':
+            result += f'Property {get_path(key_name, key)} was updated.' 
+            f'From {get_value(value)} to {get_value(value)} # тут пока затык. 
+            # Видимо надо переделывать логику добавления внутреннего представления.
+        elif meta == 'unchanged':
+            continue
         elif meta == 'added':
-            result += f'Property {key_name} was added with value: {get_value(value)}\n'
+            result += f'Property {get_path(key_name, key)}' 
+            f'was added with value: {get_value(value)}\n'
         elif meta == 'deleted':
-            result += f'Property {key_name} was removed\n'
-    return result  
+            result += f'Property {get_path(key_name, key)} was removed\n'
+    return result 
