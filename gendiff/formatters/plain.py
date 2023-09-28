a2faeee4e4459904e1654f3caea1a9164f93659b
Bkorob <bakatest311@yandex.ru>
2023-09-28 03:12:09 +0300

    ШАг7: plain

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -4,32 +4,35 @@ from gendiff.formatters.stylish import convert_value
 def get_value(value):
     if isinstance(value, dict):
         return '[complex value]'
+    if isinstance(value, str):
+        return f"'{value}'"
     return convert_value(value)
 
 
-def get_path(previous_path, new_part):
-    new_path = f'{previous_path}.{new_part}'
-    return new_path
+def get_path(previous_path, new_level):
+    return f'{previous_path}.{new_level}'[1:]
 
 
-def plain(data, key_name=''):
+def get_plain(data, key_name=''):
     result = ''
     for elem in data:
         key = elem['key']
         value = elem['value']
         meta = elem['meta']
         if meta == 'children':
-            result += plain(value, key_name=elem['key'])
-        elif meta == 'changed+' or meta == 'changed-':
-            result += (f'Property {get_path(key_name, key)} was updated.'
-                       f'From {get_value(value)} to {get_value(value)}')
-            # тут пока затык.
-            # Видимо надо переделывать логику добавления внутреннего представления.
-        elif meta == 'unchanged':
-            continue
+            result += get_plain(value, key_name + f".{key}")
+        elif meta == 'changed':
+            result += (f"Property '{get_path(key_name, key)}' was updated." 
+                       f" From {get_value(value[0])} to {get_value(value[1])}\n")
         elif meta == 'added':
-            result += f'Property {get_path(key_name, key)}'
-            f'was added with value: {get_value(value)}\n'
+            result += (f"Property '{get_path(key_name, key)}'" 
+                       f" was added with value: {get_value(value)}\n")
         elif meta == 'deleted':
-            result += f'Property {get_path(key_name, key)} was removed\n'
+            result += f"Property '{get_path(key_name, key)}' was removed\n"
     return result
+
+
+def plain(data):
+    val = get_plain(data, key_name='')
+    return val.rstrip()
+
