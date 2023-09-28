873185d645116d5d77c5032dff61d81fca91f321
Bkorob <bakatest311@yandex.ru>
2023-09-28 03:13:21 +0300

    fix bug

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,4 +1,5 @@
 from .generate import generate_diff
 from .cli import parse_arguments
 from .formatters.stylish import stylish
-__all__ = ('generate_diff', 'parse_arguments', 'stylish')
+from .formatters.plain import plain
+__all__ = ('generate_diff', 'parse_arguments', 'stylish', 'plain')
