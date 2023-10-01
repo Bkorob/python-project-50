7e64e1ce428264adfa7160299868d65ed719b750
Bkorob <bakatest311@yandex.ru>
2023-10-01 22:13:41 +0300

    Шаг8: дописан get_format.py

--- gendiff/get_format.py
+++ gendiff/get_format.py
@@ -1,8 +1,10 @@
 from .formatters.stylish import stylish
 from .formatters.plain import plain
-
+from .formatters.json import json
 
 def get_format(format):
     if format == 'plain':
         return plain
+    if format == 'json':
+        return json
     return stylish
