18fb1a2330dc1918a30044ab2bad4d25f2e658ff
Bkorob <bakatest311@yandex.ru>
2023-10-02 02:23:05 +0300

    fixed linter bugs

--- gendiff/get_format.py
+++ gendiff/get_format.py
@@ -2,6 +2,7 @@ from .formatters.stylish import stylish
 from .formatters.plain import plain
 from .formatters.json import get_json
 
+
 def get_format(format):
     if format == 'plain':
         return plain
